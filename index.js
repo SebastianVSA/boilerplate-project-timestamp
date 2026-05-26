b563495c50b4b837ccd20caf18d65bc083bf69ef
sebastian <zsebastian43212@gmail.com>
2026-05-26 15:07:55 -0400

    Update index.js
    Signed-off-by: sebastian <zsebastian43212@gmail.com>

--- index.js
+++ index.js
@@ -26,6 +26,58 @@ app.get("/api/hello", function (req, res) {
 
 
 
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+app.get("/api/:date?", function (req, res) {
+  const dateParam = req.params.date;
+
+  // Usar la fecha actual si esta vacio
+  if (!dateParam) {
+    const current = new Date();
+    return res.json({
+      unix: current.getTime(),
+      utc: current.toUTCString()
+    });
+  }
+
+  let dateObj;
+
+  // timestamp Unix
+  if (/^\d+$/.test(dateParam)) {
+    dateObj = new Date(parseInt(dateParam));
+  } else {
+    // 3. Si es una cadena de texto (ej. "2015-12-25")
+    dateObj = new Date(dateParam);
+  }
+
+  // Validador de fecha
+  if (dateObj.toString() === "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  // JSON
+  return res.json({
+    unix: dateObj.getTime(),
+    utc: dateObj.toUTCString()
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
